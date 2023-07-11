aee547eab9363d8fec9056b51ad01bb6bb8b9de0
jonatasperaza <jonatassilvaperaza@gmail.com>
2023-07-11 17:04:29 -0300

    menu e header

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//foto de perfil e usuario logado//
+let usuarioLogado = sessionStorage.getItem("usuarioLogado");
+        let imagemPerfil = sessionStorage.getItem("imagemPerfil");  
+        if (usuarioLogado && imagemPerfil) {
+            document.getElementById("perfil").innerHTML += `
+                <button class="btn-header" onclick="irperfil()" ><img src="${imagemPerfil}" alt="Imagem de Perfil" class="imagem-perfil-header centralizar perfil-header btn-header"></button>
+            `;
+        } else {
+            window.location.href = "login.html";
+        }
+////////////////////////////////////////////////////////////////
+let menu = document.getElementById("menu");
+function abrirmenu() {
+    menu.style.display = "block";
+}
