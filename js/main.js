19c37169ba35c2290c1415f66ff6114d42dd3a29
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-07-12 00:41:42 -0300

    sdfg

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let usuarioLogado = sessionStorage.getItem("usuarioLogado");
 let imagemPerfil = sessionStorage.getItem("imagemPerfil");
 if (usuarioLogado && imagemPerfil) {
   document.getElementById("perfil").innerHTML += `
-                <button class="btn-header" id="btnmenu" onclick="irperfil()" ><img src="${imagemPerfil}" alt="Imagem de Perfil" class="imagem-perfil-header centralizar perfil-header btn-header"></button>
+                <button class="btn-header" id="btnmenu" onclick="irperfil()" ><img src="${imagemPerfil}" alt="Imagem de Perfil" class="imagem-perfil-header centralizar perfil-header  btn-header"></button>
             `;
 } else {
   window.location.href = "Index.html";
