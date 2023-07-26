c574058e5f42e1ae831b7b535c39ab531f093824
jonatasperaza <jonatassilvaperaza@gmail.com>
2023-07-26 15:15:17 -0300

    26/06

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //foto de perfil e usuario logado//
+let usuario = sessionStorage.getItem("usuario");
+let senha = sessionStorage.getItem("senha");
 let usuarioLogado = sessionStorage.getItem("usuarioLogado");
 let imagemPerfil = sessionStorage.getItem("imagemPerfil");
 if (usuarioLogado && imagemPerfil) {
   document.getElementById("perfil").innerHTML += `
-                <button class="btn-header" id="btnmenu" onclick="irperfil()" ><img src="${imagemPerfil}" alt="Imagem de Perfil" class="imagem-perfil-header centralizar perfil-header  btn-header" id="foto-perfil"></button>
+                <a href="perfil.html"><button class="btn-header" id="btnmenu" onclick="irperfil()" ><img src="${imagemPerfil}" alt="Imagem de Perfil" class="imagem-perfil-header centralizar perfil-header  btn-header" id="foto-perfil"></button><\a>
             `;
 } else {
   window.location.href = "Index.html";
@@ -17,4 +19,7 @@ function abrirmenu() {
   else {
     menu.style.display = "block";
   }
+}
+function ExibirPerfil() {
+
 }
