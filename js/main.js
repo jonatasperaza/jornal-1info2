6ca1e3c28ab1a2eab52197c89cd43bd197d338f2
jonatasperaza <jonatassilvaperaza@gmail.com>
2023-08-16 15:01:00 -0300

    tentativafail

--- js/main.js
+++ js/main.js
@@ -1,40 +1,34 @@
-let senhaLogada = sessionStorage.getItem("senhaLogada")
-let usuario = sessionStorage.getItem("usuario");
-let senha = sessionStorage.getItem("senha");
+let senhaLogada = sessionStorage.getItem("senhaLogada");
 let usuarioLogado = sessionStorage.getItem("usuarioLogado");
 let imagemPerfil = sessionStorage.getItem("imagemPerfil");
+
 if (usuarioLogado && imagemPerfil) {
-  document.getElementById("perfil").innerHTML += `
-                <a href="perfil.html"><button class="btn-header" id="btnmenu" onclick="irperfil()" ><img src="${imagemPerfil}" alt="Imagem de Perfil" class="imagem-perfil-header centralizar perfil-header  btn-header" id="foto-perfil"></button><\a>
-            `;
+    document.getElementById("perfil").innerHTML += `
+        <a href="perfil.html"><button class="btn-header" id="btnmenu" onclick="irperfil()"><img src="${imagemPerfil}" alt="Imagem de Perfil" class="imagem-perfil-header centralizar perfil-header btn-header" id="foto-perfil"></button></a>
+    `;
 } else {
-  window.location.href = "Index.html";
+    window.location.href = "Index.html";
 }
 
-let menu = document.getElementById("menu");perfil
+let menu = document.getElementById("menu");
+
 function abrirmenu() {
-  if (menu.style.display == "block"){
-    menu.style.display = "none";
-
-  }
-  else {
-    menu.style.display = "block";
-  }
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
 }
+
 function ExibirPerfil() {
-  if (usuarioLogado && imagemPerfil) {
-    const ftnmse = document.getElementById("ftnmse");
-    if (ftnmse.style.display == "block") {
-      ftnmse.style.display = "none";
+    if (usuarioLogado && imagemPerfil) {
+        const ftnmse = document.getElementById("ftnmse");
+        if (ftnmse.style.display == "block") {
+            ftnmse.style.display = "none";
+        } else {
+            ftnmse.style.display = "block";
+            document.getElementById("perfil-foto").innerHTML = `<img src="${imagemPerfil}" alt="" id="perfil-foto-">`;
+            document.getElementById("perfil-nome").innerHTML = `<p>${usuarioLogado}</p>`;
+        }
     }
-    else {
-      ftnmse.style.display = "block";
-      document.getElementById("perfil-foto").innerHTML = `<img src="${imagemPerfil}" alt="" id="perfil-foto-">`
-      document.getElementById("perfil-nome").innerHTML = `<p>${usuarioLogado}<\p>`
-      document.getElementById("perfil-senha").innerHTML = `<p>${senhaLogada}<\p>`
-    }
-    
-  }
 }
-
-
